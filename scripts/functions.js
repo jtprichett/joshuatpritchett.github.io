fb555149cd512b8911d8ade6752f6a483c9af9ba
JoshuaTPritchett <jpritchett757@gmail.com>
2019-02-03 16:55:08 -0500

    functions test

---
+++ scripts/functions.js
@@ -0,0 +1,12 @@
+/*
+Functions test
+*/
+
+function multiply(num1, num2) {
+  var result = num1 * num2;
+  return result;
+}
+
+console.log(multiply(4,7));
+console.log(multiply(20,20));
+console.log(multiply(.5, 4));
