e35c5e06faf2615064f72ac9352576b76fa8f64c
JoshuaTPritchett <jpritchett757@gmail.com>
2019-02-03 16:51:56 -0500

    learning conditionals

---
+++ scripts/conditionals.js
@@ -0,0 +1,10 @@
+/*
+This is a script for me to test conditionals
+*/
+var my_name = "Jprime";
+if (my_name === "Jprime") {
+  alert(' Your nickname is JPRIME!');
+}
+else {
+  alert('Awwww, you wish it was true!');
+}
