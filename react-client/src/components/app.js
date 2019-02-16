48cf550cd999a19ccc7d5f785b3a45963391d74d
Joshua T. Pritchett <jprime@Joshuas-MacBook-Pro.local>
2019-02-16 16:01:29 -0500

    First react app running:

---
+++ react-client/src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { browserHistory } from 'react-router';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
