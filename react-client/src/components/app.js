a2baf36a27765a084171eabe90af80380903be17
Joshua T. Pritchett <jpritchett757@gmail.com>
2019-02-16 16:24:31 -0500

    adding package-lock

--- react-client/src/components/app.js
+++ react-client/src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
+import HomePage from './pages/homePage.js';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+        <Route name="home" exact path="/" component={HomePage} />
         </div>
       </Router>
     )
