9106a48228ac5251cebea1c57c8839c44d1dbf67
Joshua T. Pritchett <jpritchett757@gmail.com>
2019-02-17 10:47:54 -0500

    TESTING, WEBSITE rolling :-)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Home from './components/Home';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <h1> test </h1>
+        </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
-import App from './components/App';
+import App from './App';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
-
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  < App />,
+  document.getElementById('root')
+);
